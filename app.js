5e96e6a69663d3b9400f04ba21d65559d221b535
enesturanx <46069685+enesturanx@users.noreply.github.com>
2018-12-22 18:30:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(process.env.PORT || 5000));
+app.listen(process.env.PORT || 5000);
 
 var clients = {};
 
